a4fcdc9c78f7fbbd3df3510375a20794e99f9d29
pe-ca <mr.gloser1@gmail.com>
2022-05-01 20:43:24 -0500

    primer sevidor

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express') //usando objeto express
+const app = express() //app de express
+const port = 3000 //puerto en el que se mostrara la app localhost:300
+
+//path inicial, respondera a la url localhost:3000
+app.get('/', (req, res) =>{
+    res.send("hello world!")
+})
+
+//inicializador de la app
+app.listen(port, () =>{
+    console.log(`Example app listening on por ${port}`)
+})
